322f8fdcaf20e786de8dd5ab8702469bd2f953ce
yarikmik <yarik.mik@mail.ru>
2021-03-15 14:15:05 +0700

    созад скрипт по созданию таблиц для бд

---
+++ 1.create_database_tables.sql
@@ -0,0 +1,118 @@
+-- создание новой базы данных
+CREATE DATABASE inventory;
+
+-- 1.таблица устройств
+CREATE TABLE devises (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  devise_name VARCHAR(100) NOT NULL COMMENT "Имя устройства",
+ -- INSERT INTO `table` (`ipv4`) VALUES (INET_ATON("127.0.0.1")) использовать для внесения ip адреса
+ -- SELECT INET_NTOA(`ipv4`) FROM `table`; для вывода из таблицы в удобном формате
+  ipv4 INT UNSIGNED COMMENT "ipv4 в виде целого числа, ",
+  location_id INT UNSIGNED NOT NULL COMMENT "Ссылка на локацию, где размещено устройство",
+  services_id INT UNSIGNED NOT NULL COMMENT "Ссылка на сервис\услугу устройства (сервис который обсесечивается устройством - почта, бд, лвс, 1С и т.п )",
+  devise_type_id INT UNSIGNED NOT NULL COMMENT "Ссылка на тип устройства",
+  devise_model_id INT UNSIGNED NOT NULL COMMENT "Ссылка на модель устройства",
+  organisation_id INT UNSIGNED NOT NULL COMMENT "Ссылка на организацию владельца",
+  status_id INT UNSIGNED NOT NULL COMMENT "Ссылка на таблицу статусов для устройства",
+  description TEXT NOT NULL COMMENT "Описание, комментарии для устройства",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Устройства";  
+
+-- 2.таблица локаций
+CREATE TABLE location (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  location_name VARCHAR(100) NOT NULL COMMENT "Название локации",
+) COMMENT "Локации, местоположение";  
+
+
+-- 3.таблица типов устройств
+CREATE TABLE devise_type (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  name VARCHAR(100) NOT NULL COMMENT "Тип устройства",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Локации, местоположение";  
+
+-- 4.таблица моделей устройств
+CREATE TABLE devise_model (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  name VARCHAR(100) NOT NULL COMMENT "Модель устройства",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Локации, местоположение";  
+
+-- 5.таблица сервисов-услуг
+CREATE TABLE services (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  name VARCHAR(100) NOT NULL COMMENT "Название сервиса",
+  description TEXT COMMENT "Описание сервиса",
+  email VARCHAR(100) NOT NULL UNIQUE COMMENT "Почта группы",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Сервис\услуга";  
+
+-- 6.таблица статусов для устройств
+CREATE TABLE status (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  name VARCHAR(100) NOT NULL COMMENT "Название статуса",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Статус оборудования (выведен из эксплуатации, в работе, в резерве и т.п)";  
+
+-- 7.таблица организаций
+CREATE TABLE organisation (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  name VARCHAR(150) NOT NULL COMMENT "Название организации",
+  description TEXT COMMENT "Описание организации",
+  email VARCHAR(100) NOT NULL UNIQUE COMMENT "Почта организации",
+  phone VARCHAR(100) NOT NULL UNIQUE COMMENT "Телефон организации",
+  contact_user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пользователя - контактное лицо ",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Организация";  
+
+-- 8.таблица пользователей
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  first_name VARCHAR(100) NOT NULL COMMENT "Имя пользователя",
+  last_name VARCHAR(100) NOT NULL COMMENT "Фамилия пользователя",
+  email VARCHAR(100) NOT NULL UNIQUE COMMENT "Почта",
+  phone VARCHAR(100) NOT NULL UNIQUE COMMENT "Телефон",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Пользователи";  
+
+-- 9. Таблица связи пользователей и сервисов
+CREATE TABLE services_users (
+  services_id INT UNSIGNED NOT NULL COMMENT "Ссылка на сервис\услугу в которую входит пользователь",
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пользователя",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки", 
+  PRIMARY KEY (services_id, user_id) COMMENT "Составной первичный ключ"
+) COMMENT "Ответственные за услугу\сервис, связь между пользователями и услугами";
+
+-- 10. Каналы связи
+CREATE TABLE channels (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  name VARCHAR(250) NOT NULL COMMENT "Название канала",
+  services_id INT UNSIGNED NOT NULL COMMENT "Ссылка на сервис\услугу",
+  status_id INT UNSIGNED NOT NULL COMMENT "Ссылка на таблицу статусов для канала",
+  to_location_id INT UNSIGNED NOT NULL COMMENT "Ссылка на локацию, куда идет канал",
+  to_location_id INT UNSIGNED NOT NULL COMMENT "Ссылка на локацию, откуда идет канал",
+  provider_id INT UNSIGNED NOT NULL COMMENT "Ссылка на провайдера",
+  description TEXT COMMENT "Описание канала",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Статус оборудования (выведен из эксплуатации, в работе, в резерве и т.п)";  
+
+-- 11.таблица провайдеров
+CREATE TABLE providers (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  name VARCHAR(100) NOT NULL COMMENT "Название провайдера",
+  email VARCHAR(100) NOT NULL UNIQUE COMMENT "Почта",
+  phone VARCHAR(100) NOT NULL UNIQUE COMMENT "Телефон",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Провадеры";  
+
+SELECT INET_NTOA('3232235876') FROM DUAL;
---
+++ lesson3_examples.sql
@@ -0,0 +1,159 @@
+-- Урок 3. Введение в проектирование БД.
+
+-- Варианты решения ДЗ.
+
+-- Задание 1
+-- Установите СУБД MySQL. Создайте в домашней директории файл .my.cnf,
+-- задав в нем логин и пароль, который указывался при установке.
+
+-- [mysql]
+-- user=root
+-- password=somepassword
+
+-- Задание 2
+-- Создайте базу данных example, разместите в ней таблицу users, состоящую
+-- из двух столбцов, числового id и строкового name.
+CREATE DATABASE example;
+USE example;
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL,
+  name VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- Задание 3
+-- Создайте дамп базы данных example из предыдущего задания, разверните
+-- содержимое дампа в новую базу данных sample.
+
+-- Создаём дамп БД example (работаем в консоли)
+-- mysqldump example > example.sql
+
+-- Создаём БД sample
+-- mysql -e 'CREATE DATABASE sample'
+
+-- Загружаем дамп в БД sample
+-- mysql sample < example.sql
+
+-- Задание 4
+-- (по желанию) Ознакомьтесь более подробно с документацией утилиты
+-- mysqldump. Создайте дамп единственной таблицы help_keyword базы данных
+-- mysql. Причем добейтесь того, чтобы дамп содержал только первые 100
+-- строк таблицы.
+
+mysqldump mysql help_keyword --where='TRUE LIMIT 100' > help_keyword_report.sql
+
+-- Версия с сортировкой по идентификатору строки
+mysqldump mysql help_keyword --where='TRUE ORDER BY help_keyword_id LIMIT 100' > help_keyword_report.sql
+
+
+-- Создание БД для социальной сети ВКонтакте
+-- https://vk.com/geekbrainsru
+
+-- Создаём БД
+CREATE DATABASE vk;
+
+-- Делаем её текущей
+USE vk;
+
+-- Создаём таблицу пользователей
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  first_name VARCHAR(100) NOT NULL COMMENT "Имя пользователя",
+  last_name VARCHAR(100) NOT NULL COMMENT "Фамилия пользователя",
+  email VARCHAR(100) NOT NULL UNIQUE COMMENT "Почта",
+  phone VARCHAR(100) NOT NULL UNIQUE COMMENT "Телефон",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Пользователи";  
+
+-- Таблица профилей
+CREATE TABLE profiles (
+  user_id INT UNSIGNED NOT NULL PRIMARY KEY COMMENT "Ссылка на пользователя", 
+  gender CHAR(1) NOT NULL COMMENT "Пол",
+  birthday DATE COMMENT "Дата рождения",
+  city VARCHAR(130) COMMENT "Город проживания",
+  country VARCHAR(130) COMMENT "Страна проживания",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Профили"; 
+
+-- Таблица сообщений
+CREATE TABLE messages (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки", 
+  from_user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на отправителя сообщения",
+  to_user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на получателя сообщения",
+  body TEXT NOT NULL COMMENT "Текст сообщения",
+  is_important BOOLEAN COMMENT "Признак важности",
+  is_delivered BOOLEAN COMMENT "Признак доставки",
+  created_at DATETIME DEFAULT NOW() COMMENT "Время создания строки"
+) COMMENT "Сообщения";
+
+-- Таблица дружбы
+CREATE TABLE friendship (
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на инициатора дружеских отношений",
+  friend_id INT UNSIGNED NOT NULL COMMENT "Ссылка на получателя приглашения дружить",
+  friendship_status_id INT UNSIGNED NOT NULL COMMENT "Ссылка на статус (текущее состояние) отношений",
+  requested_at DATETIME DEFAULT NOW() COMMENT "Время отправления приглашения дружить",
+  confirmed_at DATETIME COMMENT "Время подтверждения приглашения",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки",  
+  PRIMARY KEY (user_id, friend_id) COMMENT "Составной первичный ключ"
+) COMMENT "Таблица дружбы";
+
+-- Таблица статусов дружеских отношений
+CREATE TABLE friendship_statuses (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  name VARCHAR(150) NOT NULL UNIQUE COMMENT "Название статуса",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"  
+) COMMENT "Статусы дружбы";
+
+-- Таблица групп
+CREATE TABLE communities (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор сроки",
+  name VARCHAR(150) NOT NULL UNIQUE COMMENT "Название группы",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"  
+) COMMENT "Группы";
+
+-- Таблица связи пользователей и групп
+CREATE TABLE communities_users (
+  community_id INT UNSIGNED NOT NULL COMMENT "Ссылка на группу",
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пользователя",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки", 
+  PRIMARY KEY (community_id, user_id) COMMENT "Составной первичный ключ"
+) COMMENT "Участники групп, связь между пользователями и группами";
+
+-- Таблица медиафайлов
+CREATE TABLE media (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  user_id INT UNSIGNED NOT NULL COMMENT "Ссылка на пользователя, который загрузил файл",
+  filename VARCHAR(255) NOT NULL COMMENT "Путь к файлу",
+  size INT NOT NULL COMMENT "Размер файла",
+  metadata JSON COMMENT "Метаданные файла",
+  media_type_id INT UNSIGNED NOT NULL COMMENT "Ссылка на тип контента",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Медиафайлы";
+
+-- Таблица типов медиафайлов
+CREATE TABLE media_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "Идентификатор строки",
+  name VARCHAR(255) NOT NULL UNIQUE COMMENT "Название типа",
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT "Время создания строки",  
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "Время обновления строки"
+) COMMENT "Типы медиафайлов";
+
+
+-- Рекомендуемый стиль написания кода SQL
+-- https://www.sqlstyle.guide/ru/
+
+-- Заполняем таблицы с учётом отношений 
+-- на http://filldb.info
+
+-- Документация
+-- https://dev.mysql.com/doc/refman/8.0/en/
+-- http://www.rldp.ru/mysql/mysql80/index.htm
+
+
+ 
